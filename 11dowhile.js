86d5d27a1cf45c88a793b7800da9a93b3d77b89d
nikhilmane81 <mane.nikhil92@gmail.com>
2022-12-04 11:11:28 +0530

    Update

---
+++ 11dowhile.js
@@ -0,0 +1,12 @@
+
+// do while loop = do something,
+//                             then check the condition,
+//                             repeat if condition is true
+
+let userName;
+
+do{
+    userName = window.prompt("Enter your name");
+}while(userName == "")
+
+console.log("Hello", userName);
